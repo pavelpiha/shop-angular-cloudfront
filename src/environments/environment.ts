7d0e2090fa2a2b8b2e461ee1732c03de20394440
Pavel Piha <ppiha@infoblox.com>
2021-08-26 21:38:06 +0300

    chore: update api paths

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://dr2zsivmna.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'change this',
-    import: 'https://fygrz3mlmf.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://f51jifxxd6.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'change this',
     cart: 'change this',
   },
